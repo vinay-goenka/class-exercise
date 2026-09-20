1ba03031b68070394ffd2b1eb70b69ba6feaa234
vinay-goenka <vinaygoenka2007@hotmail.com>
2026-09-20 17:33:59 -0400

    Implement multiply

--- class3_calculator.py
+++ class3_calculator.py
@@ -6,12 +6,15 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 def main():
     parser = argparse.ArgumentParser(description='Simple calculator')
     parser.add_argument('--a', '-a', type=float, required=True, help='First number')
     parser.add_argument('--b', '-b', type=float, required=True, help='Second number')
     parser.add_argument(
-        "--operation", "-op", choices=["add", "subtract"], default="add", help="Operation to perform"
+        "--operation", "-op", choices=["add", "subtract", "multiply"], default="add", help="Operation to perform"
     )
     args = parser.parse_args()
     result = 0.0
@@ -19,6 +22,8 @@ def main():
         result = add(args.a, args.b)
     elif args.operation == "subtract":
         result = subtract(args.a, args.b)
+    elif args.operation == "multiply":
+        result = multiply(args.a, args.b)
 
     print(f"Result: {result}")
 
